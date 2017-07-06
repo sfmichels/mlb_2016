f5c276fe367f1ca07f050d0fe2e093308d0b0e35
sfmichels <sf.michels@gmail.com>
2017-07-05 19:04:27 -0700

    Basic index is working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'player/index'
+
+  resources :players
+
+  root 'player#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
